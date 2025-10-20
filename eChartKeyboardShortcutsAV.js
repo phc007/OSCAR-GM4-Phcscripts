827f0d29dea6dc3586f785443d4d16adfa891eff
phc007 <phc@srpc.ca>
2025-10-20 16:31:56 -0400

    Update eChartKeyboardShortcutsAV.js
    added measurement groups

--- eChartKeyboardShortcutsAV.js
+++ eChartKeyboardShortcutsAV.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           EChart_KeyboardShortcuts AV
 // @namespace      Phcscript
-// @version        1.1
+// @version        1.2
 // @description Various Echart shortcut buttons (Alt+ p,t,d,l,c,m,a,r,x,o).
 // @include        *av/echart*
 // @require https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js
@@ -13,20 +13,22 @@ jQuery.noConflict();
 jQuery(document).ready(function() {
 	setTimeout(function(){
     // identify areas where an alt function causes an effect
-    jQuery('[data-rbd-draggable-id="preventions"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)').html('<u>P</u>REVENTION');
-    jQuery('[data-rbd-draggable-id="ticklers"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)').html('<u>T</u>ICKLER');
-    jQuery('[data-rbd-draggable-id="disease_registry"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)').html('<u>D</u>ISEASE REGISTRY');
-    jQuery('[data-rbd-draggable-id="consultations"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)').html('<u>C</u>ONSULTATION');
-    jQuery('[data-rbd-draggable-id="medications"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)').html('MEDICATION <u>Rx</u>');
-    jQuery('[data-rbd-draggable-id="documents"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)').html('D<u>O</u>CUMENTS');
-    jQuery('[data-rbd-draggable-id="allergies"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)').html('<u>A</u>LLERGY');
+    jQuery('[data-rbd-draggable-id="preventions"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)').html('<u>P</u>revention');
+    jQuery('[data-rbd-draggable-id="ticklers"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)').html('<u>T</u>ickler');
+    jQuery('[data-rbd-draggable-id="disease_registry"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)').html('<u>D</u>isease Registry');
+    jQuery('[data-rbd-draggable-id="consultations"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)').html('<u>C</u>onsultation');
+    jQuery('[data-rbd-draggable-id="medications"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)').html('Medication <u>Rx</u>');
+    jQuery('[data-rbd-draggable-id="measurements"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)').html('<u>M</u>easurement');
+    jQuery('[data-rbd-draggable-id="documents"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)').html('D<u>o</u>cuments');
+    jQuery('[data-rbd-draggable-id="allergies"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)').html('<u>A</u>llergy');
     jQuery("span:contains('New Note')").html('<u>N</u>ew Note');
 	},3000);
 });
 
+
 document.addEventListener('keydown', function(e) {
     // keys Alt + b e f v s b t h are reserved for Firefox
-    // keys g i j k n p q s u v w y z are available to add to the below
+    // keys g j k p q s u w y z are available to add to the below
   
     if (e.code === 'KeyP' && e.altKey) {
     	console.log('Alt + P was pressed!'); 
@@ -46,7 +48,7 @@ document.addEventListener('keydown', function(e) {
     }
     if (e.code === 'KeyM' && e.altKey) {
       console.log('Alt + M was pressed!'); 
-      jQuery('[data-rbd-draggable-id="measurements"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)').trigger('click');
+      jQuery('[data-rbd-draggable-id="measurements"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > span').trigger('click');
     }
     if (e.code === 'KeyA' && e.altKey) {
       console.log('Alt + A was pressed!'); 
@@ -64,21 +66,104 @@ document.addEventListener('keydown', function(e) {
       console.log('Alt + O was pressed!'); 
       jQuery('[data-rbd-draggable-id="documents"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)').trigger('click');       
     }
+  
+  	// New Note
     if (e.code === 'KeyN' && e.altKey) {
       console.log('Alt + N was pressed!'); 
-      //jQuery('.primary').trigger('click'); //this is dangerous one 
-      jQuery("div:has(span:contains('New Note'))").trigger('click');
-             
+      jQuery("div:has(span:contains('New Note'))").trigger('click');             
     }
 
+  	// CDM Indicators
+    if (e.code === 'KeyJ' && e.altKey) {
+      console.log('Alt + J was pressed!'); 
+      jQuery('[data-rbd-draggable-id="measurements"] > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)').trigger('click');
+
+    }  
+  
+  	// a given measurement group - you can substitute your desired measurement group title for "vitals"
+    if (e.code === 'KeyI' && e.altKey) {
+      console.log('Alt + I was pressed!'); 
+      jQuery('[data-rbd-draggable-id="measurements"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > span').trigger('click');
+      setTimeout(function(){
+      	jQuery('div.measurement-group-item:contains("vitals")').trigger('click');   
+      },500);
+    }  
+ 
+  
+  	// eforms
+    if ((e.code === 'Digit1' || e.code === 'Numpad1') && e.altKey) {
+    	console.log('Alt + 1 was pressed!'); 
+      jQuery('[data-rbd-draggable-id="eforms"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)').trigger('click');
+      setTimeout(function(){
+      	jQuery('.truncate-row-1:contains("1.")').trigger('click');  // you can substitute your desired eform title for 1. here
+      },500);
+    }
+    if ((e.code === 'Digit2' || e.code === 'Numpad2') && e.altKey) {
+    	console.log('Alt + 2 was pressed!'); 
+      jQuery('[data-rbd-draggable-id="eforms"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)').trigger('click');
+      setTimeout(function(){
+      	jQuery('.truncate-row-1:contains("2.")').trigger('click');
+      },500);
+    }
+    if ((e.code === 'Digit3' || e.code === 'Numpad3') && e.altKey) {
+    	console.log('Alt + 3 was pressed!'); 
+      jQuery('[data-rbd-draggable-id="eforms"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)').trigger('click');
+      setTimeout(function(){
+      	jQuery('.truncate-row-1:contains("3.")').trigger('click');
+      },500);
+    }
+    if ((e.code === 'Digit4' || e.code === 'Numpad4') && e.altKey) {
+    	console.log('Alt + 4 was pressed!'); 
+      jQuery('[data-rbd-draggable-id="eforms"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)').trigger('click');
+      setTimeout(function(){
+      	jQuery('.truncate-row-1:contains("4.")').trigger('click');
+      },500);
+    }
+    if ((e.code === 'Digit5' || e.code === 'Numpad5') && e.altKey) {
+    	console.log('Alt + 5 was pressed!'); 
+      jQuery('[data-rbd-draggable-id="eforms"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)').trigger('click');
+      setTimeout(function(){
+      	jQuery('.truncate-row-1:contains("5.")').trigger('click');
+      },500);
+    }
+    if ((e.code === 'Digit6' || e.code === 'Numpad6') && e.altKey) {
+    	console.log('Alt + 6 was pressed!'); 
+      jQuery('[data-rbd-draggable-id="eforms"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)').trigger('click');
+      setTimeout(function(){
+      	jQuery('.truncate-row-1:contains("6.")').trigger('click');
+      },500);
+    }
+    if ((e.code === 'Digit7' || e.code === 'Numpad7') && e.altKey) {
+    	console.log('Alt + 7 was pressed!'); 
+      jQuery('[data-rbd-draggable-id="eforms"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)').trigger('click');
+      setTimeout(function(){
+      	jQuery('.truncate-row-1:contains("7.")').trigger('click');
+      },500);
+    }
+    if ((e.code === 'Digit8' || e.code === 'Numpad8') && e.altKey) {
+    	console.log('Alt + 8 was pressed!'); 
+      jQuery('[data-rbd-draggable-id="eforms"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)').trigger('click');
+      setTimeout(function(){
+      	jQuery('.truncate-row-1:contains("8.")').trigger('click');
+      },500);
+    }
+    if ((e.code === 'Digit9' || e.code === 'Numpad9') && e.altKey) {
+    	console.log('Alt + 9 was pressed!'); 
+      jQuery('[data-rbd-draggable-id="eforms"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)').trigger('click');
+      setTimeout(function(){
+      	jQuery('.truncate-row-1:contains("9.")').trigger('click');
+      },500);
+    }
+  
+ 
 });
 
 
 
 /*
+			// formats for unused "+"
  
       jQuery('[data-rbd-draggable-id="risk_factors"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)').trigger('click');
-      jQuery('[data-rbd-draggable-id="eforms"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)').trigger('click');
       jQuery('[data-rbd-draggable-id="other_meds"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)').trigger('click');
       jQuery('[data-rbd-draggable-id="labs"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)').trigger('click');
       jQuery('[data-rbd-draggable-id="social_history"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)').trigger('click');
@@ -91,8 +176,9 @@ document.addEventListener('keydown', function(e) {
       jQuery('[data-rbd-draggable-id="econsults"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)').trigger('click');
       jQuery('[data-rbd-draggable-id="disease_registry"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)').trigger('click');
       jQuery('[data-rbd-draggable-id="forms"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)').trigger('click');
-      jQuery('[data-rbd-draggable-id="eforms"] > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)').trigger('click');
+
 
  */
+
 
 
