05daac58ce34ce57e3cc8bfe619cc927a9605df0
phc007 <phc@srpc.ca>
2025-12-02 11:46:13 -0500

    Update tickleraddAV.user.js plain js
    re-factored to plain js to prevent jquery conflicts

--- tickleraddAV.user.js
+++ tickleraddAV.user.js
@@ -1,22 +1,24 @@
 // ==UserScript==
 // @name     Ticklers AV PHC 
-// @version  1.6
+// @version  1.7
 // @grant    none
 // @namespace Phcscript
 // @description Used by Macro menu commands to pass tickler detail into a new tickler
 // @updateURL https://raw.githubusercontent.com/phc007/OSCAR-GM4-Phcscripts/refs/heads/main/tickleraddAV.user.js
 // @downloadURL https://raw.githubusercontent.com/phc007/OSCAR-GM4-Phcscripts/refs/heads/main/tickleraddAV.user.js
 // @include */oscar/tickler/*
-// @require https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js
 // ==/UserScript==
 
-jQuery.noConflict();
-
-jQuery(document).ready(function() {
+window.onload = function() {
     var url = new URL(window.location.href);
     var params = url.searchParams;  
   	if (params.get("tickler")){
       console.log("Ready to insert "+params.get("tickler"));
-      jQuery('textarea[name="textarea"]').text(params.get("tickler"));
+      const textarea = document.querySelector(`textarea[name="textarea"]`);
+        if (textarea) {
+    			textarea.value = params.get("tickler");
+  			} else {
+    			console.warn(`textarea not found.`);
+  		}
     }
-});
+};
