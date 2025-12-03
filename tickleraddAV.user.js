95ea7a4730803d23290b5fde99dc728ba0c0dd24
phc007 <phc@srpc.ca>
2025-12-03 15:56:56 -0500

    Update tickleraddAV.user.js refactored
    refactored to native js, added sensible presets as used for FIT Mammo (2yr) HPV Cervix (5yr) and Colonoscopy (10yr)

--- tickleraddAV.user.js
+++ tickleraddAV.user.js
@@ -1,24 +1,95 @@
 // ==UserScript==
 // @name     Ticklers AV PHC 
-// @version  1.7
+// @version  1.8
 // @grant    none
 // @namespace Phcscript
-// @description Used by Macro menu commands to pass tickler detail into a new tickler
+// @author Peter Hutten-Czapski 2025
+// @description Native date, sensible presets, and the ability to take a passed tickler message from lab macro
 // @updateURL https://raw.githubusercontent.com/phc007/OSCAR-GM4-Phcscripts/refs/heads/main/tickleraddAV.user.js
 // @downloadURL https://raw.githubusercontent.com/phc007/OSCAR-GM4-Phcscripts/refs/heads/main/tickleraddAV.user.js
 // @include */oscar/tickler/*
 // ==/UserScript==
 
+function DateAdd(startDate, numDays, numMonths, numYears) {
+        var returnDate = new Date(startDate.getTime());
+        var yearsToAdd = numYears;
+
+        var month = returnDate.getMonth() + numMonths;
+        if (month > 11) {
+                yearsToAdd = Math.floor((month + 1) / 12);
+                month -= 12 * yearsToAdd;
+                yearsToAdd += numYears;
+        }
+        returnDate.setMonth(month);
+        returnDate.setFullYear(returnDate.getFullYear() + yearsToAdd);
+
+        returnDate.setTime(returnDate.getTime() + 60000 * 60 * 24 * numDays);
+
+        return returnDate;
+}
+
+
+function addMonth2(no) {
+        var gCurrentDate = new Date();
+        var newDate = DateAdd(gCurrentDate, 0, no, 0);
+        var newYear = newDate.getFullYear()
+        var newMonth = newDate.getMonth() + 1;
+        if (newMonth < 10) {
+                newMonth = "0" + newMonth
+        }
+        var newDay = newDate.getDate();
+        if (newDay < 10) {
+                newDay = "0" + newDay;
+        }
+        return newYear + "-" + newMonth + "-" + newDay;
+        const dateInput = document.querySelector('input[name="xml_appointment_date"]');
+
+}
+
+
+
+
 window.onload = function() {
-    var url = new URL(window.location.href);
-    var params = url.searchParams;  
-  	if (params.get("tickler")){
-      console.log("Ready to insert "+params.get("tickler"));
-      const textarea = document.querySelector(`textarea[name="textarea"]`);
-        if (textarea) {
-    			textarea.value = params.get("tickler");
-  			} else {
-    			console.warn(`textarea not found.`);
-  		}
-    }
+        const elements = document.getElementsByName("xml_appointment_date");
+        elements[0].type = "date";
+
+        const parentDiv = document.getElementById('serviceDateWrapper');
+        const newDiv = document.createElement('div');
+        const newHtml = `<div style="flex: 1 1 100%; display: flex; justify-content: flex-end; align-items: center;">
+                                <a href="#" onclick='const elements = document.getElementsByName("xml_appointment_date"); elements[0].value="` + addMonth2(2) + `"'>2m</a>
+                                <span>&nbsp; &nbsp; &nbsp;</span>
+                                <a href="#" onclick='const elements = document.getElementsByName("xml_appointment_date"); elements[0].value="` + addMonth2(6) + `"'>6m</a>
+                                <span>&nbsp; &nbsp; &nbsp;</span>
+                                <a href="#" onclick='const elements = document.getElementsByName("xml_appointment_date"); elements[0].value="` + addMonth2(12) + `"'>1y</a>
+                                <span>&nbsp; &nbsp; &nbsp;</span>
+                                <a href="#" onclick='const elements = document.getElementsByName("xml_appointment_date"); elements[0].value="` + addMonth2(60) + `"'>5y</a>
+                                <span>&nbsp; &nbsp; &nbsp;</span>  
+                                <a href="#" onclick='const elements = document.getElementsByName("xml_appointment_date"); elements[0].value="` + addMonth2(120) + `"'>10y</a>
+                                <span>&nbsp; &nbsp; &nbsp;</span>  
+                            </div>
+                            <div class="error-message my-2" id="errorServiceDate"></div>`;
+
+        const oldFirstChildDiv = parentDiv.children[2];
+        newDiv.innerHTML = newHtml;
+
+        if (oldFirstChildDiv) { // Check if there is a first child to replace
+                parentDiv.replaceChild(newDiv, oldFirstChildDiv);
+        } else {
+                parentDiv.appendChild(newDiv); // If no first child, just append the new one
+        }
+
+        var url = new URL(window.location.href);
+        var params = url.searchParams;
+        console.log(params);
+        if (params.get("tickler")) {
+                console.log("Ready to insert " + params.get("tickler"));
+                const textarea = document.querySelector(`textarea[name="textarea"]`);
+                if (textarea) {
+                        textarea.value = params.get("tickler");
+                } else {
+                        console.warn(`textarea not found.`);
+                }
+        }
+
+
 };
