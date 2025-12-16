478c7e4baa7113b1e44f5b7cfa3cc06527ae4420
phc007 <11081221+phc007@users.noreply.github.com>
2025-12-16 11:54:41 -0500

    Update PreventionsAV.user.js
    remove sha

--- PreventionsAV.user.js
+++ PreventionsAV.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name     Preventions PHC
 // @author	 Peter Hutten-Czapski
-// @version  1.2.5
+// @version  1.2.6
 // @grant    none
 // @namespace Phcscript
 // @description Adds a menu of all the immunization brands available in Canada to AV
 // @updateURL https://raw.githubusercontent.com/phc007/OSCAR-GM4-Phcscripts/refs/heads/main/PreventionsAV.user.js
 // @downloadURL https://raw.githubusercontent.com/phc007/OSCAR-GM4-Phcscripts/refs/heads/main/PreventionsAV.user.js
 // @include https://app.avaros.ca/oscar/oscarPrevention/index.jsp*
-// @require https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js#sha512=MXe5EK5gyK+fbhwQy/dukwz9fw71HZcsM4KsyDBDTvMyjymkiO0M5qqU0lF4vqLI4VnKf1+DIKf1GM6RFkO8PA==
-// @require https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.14.1/jquery-ui.min.js#sha512=MSOo1aY+3pXCOCdGAYoBZ6YGI0aragoQsg1mKKBHXCYPIWxamwOE7Drh+N5CPgGI5SA9IEKJiPjdfqWFWmZtRA==    
+// @require https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js
+// @require https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.14.1/jquery-ui.min.js
 // ==/UserScript==
 
 jQuery.noConflict();
