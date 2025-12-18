a86738c7d8f9da3507e68b8ae438ddc550849365
phc007 <11081221+phc007@users.noreply.github.com>
2025-12-17 19:56:58 -0500

    Update tickleraddAV.user.js
    refactored the unreliable onload

--- tickleraddAV.user.js
+++ tickleraddAV.user.js
@@ -1,19 +1,18 @@
 // ==UserScript==
-// @name     Ticklers AV PHC 
-// @version  2.0
+// @name     Ticklers AV PHC
+// @author   Peter Hutten-Czapski 2025
+// @version  2.1
 // @grant    none
 // @namespace Phcscript
-// @author Peter Hutten-Czapski 2025
 // @description Native date, sensible presets, and the ability to take a passed tickler message from lab macro
 // @updateURL https://raw.githubusercontent.com/phc007/OSCAR-GM4-Phcscripts/refs/heads/main/tickleraddAV.user.js
 // @downloadURL https://raw.githubusercontent.com/phc007/OSCAR-GM4-Phcscripts/refs/heads/main/tickleraddAV.user.js
-// @include */oscar/tickler/*
+// @include *tickler*
 // ==/UserScript==
 
 function DateAdd(startDate, numDays, numMonths, numYears) {
         var returnDate = new Date(startDate.getTime());
         var yearsToAdd = numYears;
-
         var month = returnDate.getMonth() + numMonths;
         if (month > 11) {
                 yearsToAdd = Math.floor((month + 1) / 12);
@@ -22,12 +21,9 @@ function DateAdd(startDate, numDays, numMonths, numYears) {
         }
         returnDate.setMonth(month);
         returnDate.setFullYear(returnDate.getFullYear() + yearsToAdd);
-
         returnDate.setTime(returnDate.getTime() + 60000 * 60 * 24 * numDays);
-
         return returnDate;
 }
-
 
 function addMonth2(no) {
         var gCurrentDate = new Date();
@@ -43,20 +39,48 @@ function addMonth2(no) {
         }
         return newYear + "-" + newMonth + "-" + newDay;
         const dateInput = document.querySelector('input[name="xml_appointment_date"]');
+}
+console.log("this window is included in the Tickler selection");
 
+function waitForDocumentComplete({ timeout = 5000 } = {}) {
+  return new Promise(resolve => {
+    // Already complete → run immediately
+    if (document.readyState === 'complete') {
+      resolve({ reason: 'ready' });
+      return;
+    }
+
+    let settled = false;
+
+    const done = (reason) => {
+      if (settled) return;
+      settled = true;
+      clearTimeout(timer);
+      window.removeEventListener('load', onLoad);
+      resolve({ reason });
+    };
+
+    const onLoad = () => done('load');
+
+    // Normal path
+    window.addEventListener('load', onLoad, { once: true });
+
+    // Fallback timeout
+    const timer = setTimeout(() => done('timeout'), timeout);
+  });
 }
 
+waitForDocumentComplete({ timeout: 7000 }).then(({ reason }) => {
+  console.log('Running after:', reason);
+  configureTickler();
+});
 
-
-
-window.onload = function() {
+function configureTickler() {
         const elements = document.getElementsByName("xml_appointment_date");
-  			const oldVal = elements[0].value;
-  			if (oldVal.length > 10) {elements[0].value = oldVal.substring(0,10);}
-				elements[0].type = "date";
-
+        const oldVal = elements[0].value;
+        if (oldVal.length > 10) {elements[0].value = oldVal.substring(0,10);}
+        elements[0].type = "date";
         const parentDiv = document.getElementById('serviceDateWrapper');
-
         const newDiv = document.createElement('div');
         const newHtml = `<div style="flex: 1 1 100%; display: flex; justify-content: flex-end; align-items: center;">
                                 <a href="#" onclick='const elements = document.getElementsByName("xml_appointment_date"); elements[0].value="` + addMonth2(2) + `"'>2m</a>
@@ -70,28 +94,26 @@ window.onload = function() {
                                 <a href="#" onclick='const elements = document.getElementsByName("xml_appointment_date"); elements[0].value="` + addMonth2(36) + `"'>3y</a>
                                 <span>&nbsp; &nbsp; &nbsp;</span>
                                 <a href="#" onclick='const elements = document.getElementsByName("xml_appointment_date"); elements[0].value="` + addMonth2(60) + `"'>5y</a>
-                                <span>&nbsp; &nbsp; &nbsp;</span>  
+                                <span>&nbsp; &nbsp; &nbsp;</span>
                                 <a href="#" onclick='const elements = document.getElementsByName("xml_appointment_date"); elements[0].value="` + addMonth2(120) + `"'>10y</a>
-                                <span>&nbsp; &nbsp; &nbsp;</span>  
+                                <span>&nbsp; &nbsp; &nbsp;</span>
                             </div>
                             <div class="error-message my-2" id="errorServiceDate"></div>`;
-				newDiv.innerHTML = newHtml;
- 				if (parentDiv){                 
+        newDiv.innerHTML = newHtml;
+        if (parentDiv){
           const oldFirstChildDiv = parentDiv.children[2];
-          
-
           if (oldFirstChildDiv) { // Check if there is a first child to replace
                   parentDiv.replaceChild(newDiv, oldFirstChildDiv);
           } else {
                   parentDiv.appendChild(newDiv); // If no first child, just append the new one
           }
         } else {
- 					// for ticklerEdit.jsp         
+          // for ticklerEdit.jsp
           elements[0].removeAttribute("readonly");
-          const oldDiv = elements[0].nextElementSibling;          
+          const oldDiv = elements[0].nextElementSibling;
           elements[0].style.width = '250px';
           elements[0].style.height = '32px';
-					oldDiv.replaceWith(newDiv);          
+          oldDiv.replaceWith(newDiv);
         }
 
         var url = new URL(window.location.href);
@@ -106,6 +128,5 @@ window.onload = function() {
                         console.warn(`textarea not found.`);
                 }
         }
-
 
 };
