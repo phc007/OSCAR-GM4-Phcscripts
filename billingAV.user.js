d59dfa3dbccf4b6efe58bcb056bba7cf3b52b9f7
phc007 <11081221+phc007@users.noreply.github.com>
2025-12-17 20:11:55 -0500

    Update billingAV.user.js
    minor editing formatting

--- billingAV.user.js
+++ billingAV.user.js
@@ -1,29 +1,28 @@
 // ==UserScript==
 // @name     Billing2 PHC AV
-// @version  2.2
+// @author   Peter Hutten-Czapski
+// @version  2.3
 // @description Bills passed billing code in URL parameters.  Used in some Lab buttons or macros
 // @grant    none
 // @namespace Phcscript
 // @updateURL https://raw.githubusercontent.com/phc007/OSCAR-GM4-Phcscripts/refs/heads/main/billingAV.user.js
 // @downloadURL https://raw.githubusercontent.com/phc007/OSCAR-GM4-Phcscripts/refs/heads/main/billingAV.user.js
 // @include */av/billing2/invoice/*
-// @require https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js 
-// @require https://gist.githubusercontent.com/BrockA/2625891/raw/9c97aa67ff9c5d56be34a55ad6c18a314e5eb548/waitForKeyElements.js
+// @require https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js
+// @require https://raw.githubusercontent.com/phc007/OSCAR-GM4-Phcscripts/refs/heads/main/waitForKeyElements.js
 // ==/UserScript==
-
 
 var url = new URL(window.location.href);
 var params = url.searchParams;
 
-
-if (params.get("bill")){ // check for non empty, null, undefined, 0, false, NaN 
+if (params.get("bill")){ // check for non empty, null, undefined, 0, false, NaN
 	waitForKeyElements("#service-code-select", insertMyText, true);
 }
 
 function insertMyText() {
-  jQuery('h6').text("Billing "+params.get("bill")); 
+  jQuery('h6').text("Billing "+params.get("bill"));
   const textel = document.getElementById('service-code-select');
-  setTimeout(function(){ 
+  setTimeout(function(){
   textel.focus(); // Focus the element before executing the command
   document.execCommand('insertText', false, params.get("bill"));  //ancient and deprecated but may just work
     },3000);
